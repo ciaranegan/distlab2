5a455953b22c374e81643da7809274f744e08ae4
Ciaran Egan <ciaranegan93@gmail.com>
2014-11-24 15:57:35 +0000

    Random stuff

--- pool_server.rb
+++ pool_server.rb
@@ -5,32 +5,25 @@ class ThreadPoolServer
 
 	def initialize(size, port_no)
 		# Number of threads in the thread pool
-		@port_no = port_no
-		puts "hdello"
 		@size = size
 		# Queue of tasks for the threads to execute
 		@jobs = Queue.new 
 		# Create an array of threads
+		@port_no = port_no
 		@pool = Array.new(@size) do |i|
 			Thread.new do
 				Thread.current[:id] = i # Give each thread an ID for easy access later
 				loop do
 					client, message = @jobs.pop # Get a job from the queue
 					if message == "KILL_SERVICE\n"
-						puts "client kill service"
 						client.puts "Server shutdown"
-						self.shutdown
 					elsif message[0,5] == "HELO "
 						# Get the incoming sockets info and send it back
-						# port, ip = Socket.unpack_sockaddr_in(@server.peeraddr)
-						# sock_domain, remote_port, remote_hostname, remote_ip = @server.peeraddr
 						local_ip = UDPSocket.open {|s| s.connect("64.233.187.99", 1); s.addr.last}
 						client.puts "#{message}IP:#{local_ip}\nPort:#{@port_no}\nStudentID:11450212"
-						puts "client helo message"
 					else
 						# This catches the other messages
 						client.puts "Invalid message"
-						puts "client invalid message"
 					end
 					client.close
 				end
@@ -48,12 +41,13 @@ class ThreadPoolServer
 	end
 
 	def shutdown
+		sleep(0.5)
 		# Kills all threads in the thread pool and closes the server
 		@size.times do |i|
 			Thread.kill(@pool[i])
 		end
-		puts "Server shutdown..."
 		@server.close
+		puts "Server shutdown"
 	end
 
 	def run
@@ -66,10 +60,8 @@ class ThreadPoolServer
 			# Updates the loop condition based on the message
 			@server_running = (message != "KILL_SERVICE\n")
 		end
-		puts "outside loop"
 		# Shutsdown once a kill_service message is received
-		# self.shutdown
-		# @server.close
+		self.shutdown
 	end
 end
 
